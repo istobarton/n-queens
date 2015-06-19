67cd6aec629c1fcd9a11bccb634e5b39a84da1a4
istobarton <istobarton@gmail.com>
2015-06-18 19:48:46 -0500

    Passes n-Queens

--- src/solvers.js
+++ src/solvers.js
@@ -18,7 +18,7 @@ window.findNRooksSolution = function(n) {
     for(var i = 0; i < n; i++) {
       solution.togglePiece(i,i);
     }
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution.rows();
 };
 
@@ -26,50 +26,94 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var board = new Board({n:n});
-  var sC = 0; //fixme
-  var rC = 0;
-  var cC = 0;
-  var recurse = function(rC, cC){
-    board.togglePiece(rC, cC);
-      //if rC>n {return sC}
-      if(rC>n){
-        console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-        return sC;
-      }else if(board.hasRowConflictAt(rC)){
-        rC++;
-        recurse(rC,cC);
-        console.log("1" + rC + cC + sC)
-      }else if(board.hasColConflictAt(cC)){
-        cC++;
-        recurse(rC,cC);
-        console.log("2" + rC + cC + sC)
-      }else if(!board.hasColConflictAt(cC) && !board.hasRowConflictAt(rC)){
-        sC++;
-        rC++;
-        recurse(rC,cC);
-        console.log("3" + rC + cC + sC)
-      }
+//initiated new Board & counter;
+var counter = 0;
+var board = new Board({n:n});
+//create function
+var recurse = function(row){
+  //base case of if (row === n)(count++)
+  if(row===n) {
+    return counter++;
+  }
+  //for loop that iterates over each column
+  for(var col = 0; col<n; col++){
+    board.togglePiece(row, col);
+    if(!board.hasAnyRooksConflicts()){
+      recurse(row+1);
     }
-  // debugger;
-  recurse(rC, cC);
-};
-
-
+      board.togglePiece(row, col);
+  }
+}
+recurse(0);
+//return solution count
+return counter;
+}
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n:n});
+  var SHOW = [];
+  //create recursive function that take row
+  var recurse = function(row){
+    if(row === n){
+  //if the row equals n
+    //return solution.rows();
+      for(var i = 0; i<solution.rows().length; i++){
+        SHOW.push(solution.get(i).slice());
+        // var SHOW = _.map(solution.rows(), function(x){return x})
+      }
+      return;
+    }
+    //iterate over the columns less than n
+    for(var col = 0; col < n; col++){
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+      //toggle the row/column we're at
+      solution.togglePiece(row, col);
+      //if there are NO Queen Conflicts
+      if(!solution.hasAnyQueensConflicts()){
+        //recurse over the next row
+        recurse(row+1);
+        if(SHOW.length>0){return}
+      }
+      //else toggle piece
+      solution.togglePiece(row, col);
+
+    }
+  }
+
+  //call recursive function with row 0
+  recurse(0);
+  if(SHOW.length>0){return SHOW}
+  else{return solution.rows()}
 };
 
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solution = new Board({n:n});
+  var sC = 0;
+  //create recursive function that take row
+  var recurse = function(row){
+    if(row === n){
+  //if the row equals n
+    //return solution.rows();
+      return sC++;
+    }
+    //iterate over the columns less than n
+    for(var col = 0; col < n; col++){
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+      //toggle the row/column we're at
+      solution.togglePiece(row, col);
+      //if there are NO Queen Conflicts
+      if(!solution.hasAnyQueensConflicts()){
+        //recurse over the next row
+        recurse(row+1);
+      }
+      //else toggle piece
+      solution.togglePiece(row, col);
+
+    }
+  }
+  recurse(0);
+  return sC;
 };
